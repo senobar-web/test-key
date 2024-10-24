a4d645f0d0e9568273a20eb6a82c0a4a2c14686d
asal <assal.senobarnya@gmail.com>
2024-10-25 03:21:14 +0330

    add new project

---
+++ script.js
@@ -0,0 +1,18 @@
+// import axios from "axios";
+let btn = document.querySelector("button")
+// btn.addEventListener("click",()=>{
+
+//     axios.get('https://fakestoreapi.com/products?limit=5')
+//     .then(res => {console.log(res)
+
+
+
+//     })
+
+
+// })
+btn.addEventListener("click", () => {
+    console.log("click");
+
+})
+
